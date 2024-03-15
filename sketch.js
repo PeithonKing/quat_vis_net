03ca5dbef8704963d8ec1dac19d84fca9f7876f2
PeithonKing <aritra.mukhopadhyay@niser.ac.in>
2024-03-15 22:28:33 +0530

    started matrices

--- sketch.js
+++ sketch.js
@@ -6,14 +6,25 @@ let rout = 4;
 let scale = 1
 
 WHITE = [255, 255, 255]
-BLACK = [0, 0, 0]
-RED = [255, 0, 0]
-GREEN = [0, 255, 0]
-BLUE = [0, 0, 255]
-RED = [255, 0, 0]
-RED = [255, 0, 0]
-RED = [255, 0, 0]
-RED = [255, 0, 0]
+BLACK = [  0,   0,   0]
+RED =   [255,   0,   0]
+GREEN = [  0, 255,   0]
+BLUE =  [  0,   0, 255]
+
+Cr = "#d94179"  // 233, 30, 99
+Ci = "#459be6"  // 66, 165, 245
+Cj = "#ff774d"  // 255, 138, 101
+Ck = "#3dccb4"  // 76, 175, 80
+
+Cr_light = "#F48FB1"  // 244, 143, 177
+Ci_light = "#90CAF9"  // 144, 202, 249
+Cj_light = "#FFAB91"  // 255, 171, 145
+Ck_light = "#81C784"  // 129, 200, 132
+
+// RED = [255, 0, 0]
+// RED = [255, 0, 0]
+// RED = [255, 0, 0]
+// RED = [255, 0, 0]
 
 console.log("sketch.js is loaded");
 
@@ -43,45 +54,50 @@ class Node {
 }
 
 class Edge {
-    constructor(node1, node2, width=0.1, color=BLACK, highlight_color=GREEN, weight_coordinate=[0, 0]) {
+    constructor(node1, node2, width=1, color=BLACK, highlight_color=BLUE, weight_coordinate=[0, 0]) {
         this.node1 = node1;
         this.node2 = node2;
         this.width = width;
         this.color = color;
         this.highlight_color = highlight_color;
-        
+
         // Calculate vertices
         this.calculateVertices();
-        this.draw(false);
+        this.draw();
     }
 
-    draw(highlighted = false) {
-        // Highlight or de-highlight the rectangle
-        if (highlighted) {
-            // Highlight
-            fill(this.highlight_color);
-        } else {
-            // De-highlight
-            fill(this.color);
-        }
+    highlight() {
+        fill(this.highlight_color);
+        stroke(this.highlight_color);
+        this.draw(true);
+    }
+
+    draw() {
+        fill(this.color);
+        stroke(this.color);
         quad(this.v1.x, this.v1.y, this.v2.x, this.v2.y, this.v3.x, this.v3.y, this.v4.x, this.v4.y);
     }
 
     calculateVertices() {
         // Calculate slope
-        
-        
+
         // Calculate tan(theta), sin(theta), and cos(theta)
         var tan_theta = -(this.node2.y - this.node1.y) / (this.node2.x - this.node1.x);
         var sin_theta = tan_theta / Math.sqrt(1 + tan_theta**2);
         var cos_theta = 1 / Math.sqrt(1 + tan_theta**2);
-        
-        console.log(tan_theta, sin_theta, cos_theta);
+
+        // console.log(tan_theta, sin_theta, cos_theta);
         // Calculate the coordinates of the vertices
-        this.v1 = { x: this.node1.x - this.width/2 * cos_theta, y: this.node1.y - this.width/2 * sin_theta };
-        this.v2 = { x: this.node1.x + this.width/2 * cos_theta, y: this.node1.y + this.width/2 * sin_theta };
-        this.v3 = { x: this.node2.x + this.width/2 * cos_theta, y: this.node2.y + this.width/2 * sin_theta };
-        this.v4 = { x: this.node2.x - this.width/2 * cos_theta, y: this.node2.y - this.width/2 * sin_theta };
+        this.v1 = { x: this.node1.x - this.width/2 * sin_theta, y: this.node1.y - this.width/2 * cos_theta };
+        this.v2 = { x: this.node1.x + this.width/2 * sin_theta, y: this.node1.y + this.width/2 * cos_theta };
+        this.v3 = { x: this.node2.x + this.width/2 * sin_theta, y: this.node2.y + this.width/2 * cos_theta };
+        this.v4 = { x: this.node2.x - this.width/2 * sin_theta, y: this.node2.y - this.width/2 * cos_theta };
+
+        this.hit_v1 = { x: this.node1.x - this.width * sin_theta, y: this.node1.y - this.width * cos_theta };
+        this.hit_v2 = { x: this.node1.x + this.width * sin_theta, y: this.node1.y + this.width * cos_theta };
+        this.hit_v3 = { x: this.node2.x + this.width * sin_theta, y: this.node2.y + this.width * cos_theta };
+        this.hit_v4 = { x: this.node2.x - this.width * sin_theta, y: this.node2.y - this.width * cos_theta };
+
     }
 
     hit(point) {
@@ -109,6 +125,35 @@ class Edge {
 
 //     }
 // }
+
+class Matrix{
+    constructor(width, height, origin, size, color = RED, highlight_color = BLUE) {
+        this.width = width;
+        this.height = height;
+        this.origin = origin;
+        this.size = size;
+        this.color = color;
+        this.highlight_color = highlight_color;
+        this.draw(color);
+        // console.log("width = ", this.width, "height = ", this.height, "origin = ", this.origin, "size = ", this.size, "color = ", this.color);
+    }
+
+    highlight() {
+        this.draw(this.highlight_color);
+    }
+
+    draw(color) {
+        // console.log("drawing matrix");
+        fill(color);
+        stroke(0);
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                square(this.origin[0] + i * this.size, this.origin[1] + j * this.size, this.size);
+            }
+        }
+    }
+
+}
 
 function prepareNetwork(qin, qout) {
     rin = qin * 4;
@@ -146,11 +191,11 @@ function prepareNetwork(qin, qout) {
         quat_output_nodes.push(new Node(quat_origin[0]+x_spacing/2, quat_origin[1] + y_spacing * i, BLUE));
     }
 
-    console.log(real_input_nodes.length, real_output_nodes.length, quat_input_nodes.length, quat_output_nodes.length);
+    // console.log(real_input_nodes.length, real_output_nodes.length, quat_input_nodes.length, quat_output_nodes.length);
 
     real_input_nodes.forEach(node1 => {
         real_output_nodes.forEach(node2 => {
-            newedge = new Edge(node1, node2);
+            newedge = new Edge(node1, node2, 1, BLACK);
             node1.neighbours.push(node2);
             node1.edges.push(newedge);
             node2.neighbours.push(node1);
@@ -160,7 +205,7 @@ function prepareNetwork(qin, qout) {
 
     quat_input_nodes.forEach(node1 => {
         quat_output_nodes.forEach(node2 => {
-            newedge = new Edge(node1, node2);
+            newedge = new Edge(node1, node2, 1, BLACK);
             node1.neighbours.push(node2);
             node1.edges.push(newedge);
             node2.neighbours.push(node1);
@@ -169,14 +214,78 @@ function prepareNetwork(qin, qout) {
     });
 
 
+    // display a (qin, qout) dimentional matrix
+
+    // for(let i = 0; i < qin; i++) {
+    //     for(let j = 0; j < qout; j++) {
+    //         // console.log(quat_input_nodes[i].neighbours[j]);
+    //         // console.log(quat_input_nodes[i].edges[j]);
+    //         // console.log(quat_output_nodes[j].neighbours[i]);
+    //         // console.log(quat_output_nodes[j].edges[i]);
+    //     }
+    // }
+
+    size = 15;
+    spacing = 50
+    o = [300, 300]
+    // quaternion_matrix = [
+    //     [r, -i, -j, -k],
+    //     [i,  r, -k,  j],
+    //     [j,  k,  r, -i],
+    //     [k, -j,  i,  r]
+    // ]
+
+    // the four quaternion matrices
+    new Matrix(qin, qout, [o[0]        , o[1]        ], size, Cr, Cr_light)
+    new Matrix(qin, qout, [o[0]+spacing, o[1]        ], size, Ci, Ci_light)
+    new Matrix(qin, qout, [o[0]        , o[1]+spacing], size, Cj, Cj_light)
+    new Matrix(qin, qout, [o[0]+spacing, o[1]+spacing], size, Ck, Ck_light)
+    
+    o = [450, 400]
+    // the 4 * 4 = 16 matrices for real
+    // quaternion_matrix = [
+    //     [r, i, j, k],
+    //     [i, r, k, j],
+    //     [j, k, r, i],
+    //     [k, j, i, r]
+    // ]
+    new Matrix(qin, qout, [o[0]+0*spacing, o[1]+0*spacing], size, Cr, Cr_light)
+    new Matrix(qin, qout, [o[0]+1*spacing, o[1]+0*spacing], size, Ci, Ci_light)
+    new Matrix(qin, qout, [o[0]+2*spacing, o[1]+0*spacing], size, Cj, Cj_light)
+    new Matrix(qin, qout, [o[0]+3*spacing, o[1]+0*spacing], size, Ck, Ck_light)
+
+    new Matrix(qin, qout, [o[0]+0*spacing, o[1]+1*spacing], size, Ci, Ci_light)
+    new Matrix(qin, qout, [o[0]+1*spacing, o[1]+1*spacing], size, Cr, Cr_light)
+    new Matrix(qin, qout, [o[0]+2*spacing, o[1]+1*spacing], size, Ck, Ck_light)
+    new Matrix(qin, qout, [o[0]+3*spacing, o[1]+1*spacing], size, Cj, Cj_light)
+
+    new Matrix(qin, qout, [o[0]+0*spacing, o[1]+2*spacing], size, Cj, Cj_light)
+    new Matrix(qin, qout, [o[0]+1*spacing, o[1]+2*spacing], size, Ck, Ck_light)
+    new Matrix(qin, qout, [o[0]+2*spacing, o[1]+2*spacing], size, Cr, Cr_light)
+    new Matrix(qin, qout, [o[0]+3*spacing, o[1]+2*spacing], size, Ci, Ci_light)
+
+    new Matrix(qin, qout, [o[0]+0*spacing, o[1]+3*spacing], size, Ck, Ck_light)
+    new Matrix(qin, qout, [o[0]+1*spacing, o[1]+3*spacing], size, Cj, Cj_light)
+    new Matrix(qin, qout, [o[0]+2*spacing, o[1]+3*spacing], size, Ci, Ci_light)
+    new Matrix(qin, qout, [o[0]+3*spacing, o[1]+3*spacing], size, Cr, Cr_light)
+
+
+
+    // new Matrix(qin, qout, [o[0]        , o[1]        ], size, Cr_light)
+    // new Matrix(qin, qout, [o[0]+spacing, o[1]        ], size, Ci_light)
+    // new Matrix(qin, qout, [o[0]        , o[1]+spacing], size, Cj_light)
+    // new Matrix(qin, qout, [o[0]+spacing, o[1]+spacing], size, Ck_light)
+
+
 }
 
 function setup() {
     createCanvas(800*scale, 600*scale);
+    draw();
 }
 
 function draw() {
-    background(200);
+    background(255);
 
     // Update the number of neurons based on the slider value
     qin = document.getElementById("inNeurons").value;
